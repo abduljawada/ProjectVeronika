3f9f96c7d2e28964351d31699f22a3417115954d
abduljawada <125431874+abduljawada@users.noreply.github.com>
2023-03-31 23:36:59 +0400

    Allowed LAN connections

--- Assets/NetworkButtons.cs
+++ Assets/NetworkButtons.cs
@@ -2,12 +2,18 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using TMPro;
+using Unity.Netcode.Transports.UTP;
 
 public class NetworkButtons : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField ipInputField;
+    private UnityTransport Transport => GetComponent<UnityTransport>();
+    
     public void StartClient()
     {
         Debug.Log("Starting Client");
+        Transport.ConnectionData.Address = ipInputField.text;
         UnloadScene();
         NetworkManager.Singleton.StartClient();
         Destroy(Movement.Singleton.gameObject.GetComponentInChildren<Light2D>().gameObject);
