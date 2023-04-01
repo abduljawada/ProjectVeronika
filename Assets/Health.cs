dd94fd0762e3c6b4d15a7f190a870b869c4aeb91
abduljawada <125431874+abduljawada@users.noreply.github.com>
2023-04-01 14:44:00 +0400

    Upgraded to Unity 2022.2.13f

---
+++ Assets/Health.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 3;
+    private int _currentHealth;
+
+    [SerializeField] private UnityEvent onDeathEvent;
+
+    private void Awake()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    public void LoseHealth(int damage = 1)
+    {
+        _currentHealth -= damage;
+
+        if (_currentHealth <= 0)
+        {
+            onDeathEvent.Invoke();
+        }
+    }
+}
